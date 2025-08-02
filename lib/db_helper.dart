105385bb728a0a88f2923d182c0978a3f897e658
raunakpurohit <raunak.wscubetech@gmail.com>
2025-08-02 18:18:00 +0530

    update delete implemented

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -17,6 +17,13 @@ class DBHelper {
   ///Database
   Database? mDB;
 
+  ///note table
+  String noteTable = "note";
+  static final String columnNoteId = "n_id";
+  static final String columnNoteTitle = "n_title";
+  static final String columnNoteDesc = "n_desc";
+  static final String columnNoteCreatedAt = "n_created_at";
+
   ///initDB
   Future<Database> initDb() async {
     mDB ??= await openDB();
@@ -37,7 +44,7 @@ class DBHelper {
         ///create tables
 
         db.execute(
-          "create table note ( n_id integer primary key autoincrement, n_title text, n_desc text, n_created_at text)",
+          "create table $noteTable ( $columnNoteId integer primary key autoincrement, $columnNoteTitle text, $columnNoteDesc text, $columnNoteCreatedAt text)",
         );
       },
     );
@@ -46,10 +53,10 @@ class DBHelper {
   ///insert
   Future<bool> addNote({required String title, required String desc}) async{
     Database db = await initDb();
-    int rowsEffected = await db.insert("note", {
-      "n_title": title,
-      "n_desc": desc,
-      "n_created_at" : DateTime.now().millisecondsSinceEpoch.toString()
+    int rowsEffected = await db.insert(noteTable, {
+      columnNoteTitle : title,
+      columnNoteDesc : desc,
+      columnNoteCreatedAt : DateTime.now().millisecondsSinceEpoch.toString()
     });
 
     return rowsEffected > 0;
@@ -60,9 +67,27 @@ class DBHelper {
 
     Database db = await initDb();
 
-    return db.query("note");
+    return db.query(noteTable);
   }
 
   ///update
+  Future<bool> updateNote({required String updatedTitle, required String updatedDesc, required int id}) async{
+    var db = await initDb();
+
+    int rowsEffected = await db.update(noteTable, {
+      columnNoteTitle : updatedTitle,
+      columnNoteDesc : updatedDesc
+    }, where: "$columnNoteId = ?", whereArgs: ["$id"]);
+
+    return rowsEffected > 0;
+  }
   ///delete
+  Future<bool> deleteNote({required int id}) async{
+
+    var db = await initDb();
+
+    int rowsEffected = await db.delete(noteTable,where: "$columnNoteId = ?", whereArgs: ["$id"]);
+
+    return rowsEffected > 0;
+  }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -19,7 +19,7 @@ class _HomePageState extends State<HomePage> {
     getNotes();
   }
 
-  void getNotes() async{
+  void getNotes() async {
     notes = await dbHelper!.fetchAllNotes();
     setState(() {});
   }
@@ -27,94 +27,210 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: notes.isNotEmpty ? ListView.builder(
-          itemCount: notes.length,
-          itemBuilder: (_, index){
-        return ListTile(
-          title: Text(notes[index]["n_title"]),
-          subtitle: Text(notes[index]["n_desc"]),
-          trailing: SizedBox(
-            width: 100,
-            child: Row(
-              children: [
-                IconButton(onPressed: (){}, icon: Icon(Icons.edit)),
-                IconButton(onPressed: (){}, icon: Icon(Icons.delete, color: Colors.red,)),
-              ],
-            ),
-          ),
-        );
-      }) : Center(
-        child: Text('No Notes Yet!!'),
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: (){
+      appBar: AppBar(title: Text('Home')),
+      body: notes.isNotEmpty
+          ? ListView.builder(
+              itemCount: notes.length,
+              itemBuilder: (_, index) {
+                return ListTile(
+                  title: Text(notes[index][DBHelper.columnNoteTitle]),
+                  subtitle: Text(notes[index][DBHelper.columnNoteDesc]),
+                  trailing: SizedBox(
+                    width: 100,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          onPressed: () async {
+                            titleController.text =
+                                notes[index][DBHelper.columnNoteTitle];
+                            descController.text =
+                                notes[index][DBHelper.columnNoteDesc];
+                            showModalBottomSheet(
+                              context: context,
+                              isDismissible: false,
+                              enableDrag: false,
+                              builder: (_) {
+                                return bottomSheetUI(
+                                  isUpdate: true,
+                                  id: notes[index][DBHelper.columnNoteId],
+                                );
+                              },
+                            );
+                          },
+                          icon: Icon(Icons.edit),
+                        ),
+                        IconButton(
+                          onPressed: () async {
+                            showModalBottomSheet(
+                              context: context,
+                              builder: (_) {
+                                return Container(
+                                  padding: EdgeInsets.symmetric(
+                                    vertical: 30,
+                                    horizontal: 16,
+                                  ),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text(
+                                        'Are you sure want to delete?',
+                                        style: TextStyle(
+                                          fontSize: 21,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(height: 11),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: [
+                                          OutlinedButton(
+                                            onPressed: () async {
+                                              bool check = await dbHelper!
+                                                  .deleteNote(
+                                                    id:
+                                                        notes[index][DBHelper
+                                                            .columnNoteId],
+                                                  );
 
-        titleController.text = "";
-        descController.clear();
-        showModalBottomSheet(
+                                              if (check) {
+                                                getNotes();
+                                                Navigator.pop(context);
+                                              }
+                                            },
+                                            child: Text(
+                                              'Yes',
+                                              style: TextStyle(
+                                                color: Colors.red,
+                                              ),
+                                            ),
+                                            style: OutlinedButton.styleFrom(
+                                              side: BorderSide(
+                                                color: Colors.red,
+                                                width: 2,
+                                              ),
+                                            ),
+                                          ),
+                                          SizedBox(width: 11),
+                                          OutlinedButton(
+                                            style: OutlinedButton.styleFrom(
+                                              side: BorderSide(
+                                                color: Colors.green,
+                                                width: 2,
+                                              ),
+                                            ),
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: Text('No', style: TextStyle(color: Colors.green)),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                          icon: Icon(Icons.delete, color: Colors.red),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            )
+          : Center(child: Text('No Notes Yet!!')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          titleController.text = "";
+          descController.clear();
+          showModalBottomSheet(
             context: context,
             isDismissible: false,
             enableDrag: false,
-            builder: (_){
-          return Container(
-            padding: EdgeInsets.only(left: 11, right: 11, top: 11, bottom: 70),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text("Add Note", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                SizedBox(height: 10,),
-                TextField(
-                  controller: titleController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                      labelText: "Title",
-                      hintText: "Enter your title here.."
-                  ),
-                ),
-                SizedBox(height: 10,),
-                TextField(
-                  controller: descController,
-                  minLines: 4,
-                  maxLines: 4,
-                  decoration: InputDecoration(
-                    alignLabelWithHint: true,
-                    border: OutlineInputBorder(),
-                    labelText: "Desc",
-                    hintText: "Enter your desc here..",
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    OutlinedButton(onPressed: () async{
-
-                      if(titleController.text.isNotEmpty && descController.text.isNotEmpty) {
-                        bool check = await dbHelper!.addNote(title: titleController.text,
-                            desc: descController.text);
-                        if(check){
-                          getNotes();
-                          Navigator.pop(context);
-                        }
-                      }
-
-
-                    }, child: Text('Save')),
-                    SizedBox(width: 11,),
-                    OutlinedButton(onPressed: (){
-                      Navigator.pop(context);
-                    }, child: Text('Cancel')),
-                  ],
-                )
-              ],
-            ),
+            builder: (_) {
+              return bottomSheetUI();
+            },
           );
-        });
-      }, child: Icon(Icons.add),),
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget bottomSheetUI({bool isUpdate = false, int id = 0}) {
+    return Container(
+      padding: EdgeInsets.only(left: 11, right: 11, top: 11, bottom: 70),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            "${isUpdate ? "Update" : "Add"} Note",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Title",
+              hintText: "Enter your title here..",
+            ),
+          ),
+          SizedBox(height: 10),
+          TextField(
+            controller: descController,
+            minLines: 4,
+            maxLines: 4,
+            decoration: InputDecoration(
+              alignLabelWithHint: true,
+              border: OutlineInputBorder(),
+              labelText: "Desc",
+              hintText: "Enter your desc here..",
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              OutlinedButton(
+                onPressed: () async {
+                  if (titleController.text.isNotEmpty &&
+                      descController.text.isNotEmpty) {
+                    bool check = false;
+
+                    if (isUpdate) {
+                      check = await dbHelper!.updateNote(
+                        updatedTitle: titleController.text,
+                        updatedDesc: descController.text,
+                        id: id,
+                      );
+                    } else {
+                      check = await dbHelper!.addNote(
+                        title: titleController.text,
+                        desc: descController.text,
+                      );
+                    }
+                    if (check) {
+                      getNotes();
+                      Navigator.pop(context);
+                    }
+                  }
+                },
+                child: Text('Save'),
+              ),
+              SizedBox(width: 11),
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Cancel'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
